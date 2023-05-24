3be3577d4f48c70bc9ca4041cd9194207650b6fd
shitingbao <871258317@qq.com>
2023-05-24 14:09:27 +0800

    feat: add errors model

--- car/internal/service/car.go
+++ car/internal/service/car.go
@@ -4,12 +4,12 @@ import (
 	"context"
 	"log"
 
-	pb "car/api/car"
+	api_car "car/api/car"
 	"car/internal/biz"
 )
 
 type CarService struct {
-	pb.UnimplementedCarServer
+	api_car.UnimplementedCarServer
 
 	car *biz.CarUsecase
 }
@@ -20,51 +20,54 @@ func NewCarService(c *biz.CarUsecase) *CarService {
 	}
 }
 
-func (s *CarService) CreateCar(ctx context.Context, req *pb.CreateCarRequest) (*pb.CreateCarReply, error) {
+func (s *CarService) CreateCar(ctx context.Context, req *api_car.CreateCarRequest) (*api_car.CreateCarReply, error) {
 	c, err := s.car.CreateCar(ctx, &biz.Car{
 		Name: req.CarName,
 	})
-	return &pb.CreateCarReply{
+	return &api_car.CreateCarReply{
 		CarId: c.ID,
 	}, err
 }
 
-func (s *CarService) UpdateCar(ctx context.Context, req *pb.UpdateCarRequest) (*pb.UpdateCarReply, error) {
+func (s *CarService) UpdateCar(ctx context.Context, req *api_car.UpdateCarRequest) (*api_car.UpdateCarReply, error) {
 	c, err := s.car.Update(ctx, &biz.Car{
 		ID:   req.Id,
 		Name: req.Name,
 	})
-	return &pb.UpdateCarReply{
+	return &api_car.UpdateCarReply{
 		Name: c.Name,
 	}, err
 }
 
-func (s *CarService) DeleteCar(ctx context.Context, req *pb.DeleteCarRequest) (*pb.DeleteCarReply, error) {
-	return &pb.DeleteCarReply{}, nil
+func (s *CarService) DeleteCar(ctx context.Context, req *api_car.DeleteCarRequest) (*api_car.DeleteCarReply, error) {
+	return &api_car.DeleteCarReply{}, nil
 }
 
-func (s *CarService) GetCar(ctx context.Context, req *pb.GetCarRequest) (*pb.GetCarReply, error) {
+func (s *CarService) GetCar(ctx context.Context, req *api_car.GetCarRequest) (*api_car.GetCarReply, error) {
+	if req.Id == 0 {
+		return nil, api_car.ErrorUserNotFound("not id", "no this car")
+	}
 	c, err := s.car.FindByID(ctx, req.Id)
-	return &pb.GetCarReply{
+	return &api_car.GetCarReply{
 		Name: c.Name,
 	}, err
 }
 
-func (s *CarService) ListCar(ctx context.Context, req *pb.ListCarRequest) (*pb.ListCarReply, error) {
+func (s *CarService) ListCar(ctx context.Context, req *api_car.ListCarRequest) (*api_car.ListCarReply, error) {
 	log.Println("you car:", req.CreateTime.AsTime())
 	res, err := s.car.ListAll(ctx)
-	list := []*pb.CarReply{}
+	list := []*api_car.CarReply{}
 	for _, v := range res {
-		p := &pb.CarReply{
+		p := &api_car.CarReply{
 			CarName: v.Name,
 		}
 		list = append(list, p)
 	}
-	return &pb.ListCarReply{
+	return &api_car.ListCarReply{
 		List: list,
 	}, err
 }
 
-func (s *CarService) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloReply, error) {
-	return &pb.HelloReply{}, nil
+func (s *CarService) SayHello(ctx context.Context, req *api_car.HelloRequest) (*api_car.HelloReply, error) {
+	return &api_car.HelloReply{}, nil
 }
